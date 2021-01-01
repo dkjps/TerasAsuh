224ea5c99823feb90b36a6c8d6ced26a6f74644c
Khafido Ilzam <khafido.work@gmail.com>
2021-01-01 12:36:36 +0700

    manifest

--- application/views/login.php
+++ application/views/login.php
@@ -13,27 +13,27 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <link rel="stylesheet" href="<?php echo base_url(); ?>assets/modules/fontawesome/css/all.min.css">
 
   <!-- CSS Libraries -->
-<?php
-if ($this->uri->segment(2) == "" || $this->uri->segment(2) == "dashboard") { ?>
-  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/modules/jqvmap/dist/jqvmap.min.css">
-  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/modules/summernote/summernote-bs4.css">
-  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/modules/owlcarousel2/dist/assets/owl.carousel.min.css">
-  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/modules/owlcarousel2/dist/assets/owl.theme.default.min.css">
+  <?php
+  if ($this->uri->segment(2) == "" || $this->uri->segment(2) == "dashboard") { ?>
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/modules/jqvmap/dist/jqvmap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/modules/summernote/summernote-bs4.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/modules/owlcarousel2/dist/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/modules/owlcarousel2/dist/assets/owl.theme.default.min.css">
 
-<?php } elseif ($this->uri->segment(2) == "master"){ ?>
-  <link href="https://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css" rel="stylesheet" type="text/css" />
-  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/modules/datatables/datatables.min.css">
-  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/modules/datatables/DataTables-1.10.16/css/dataTables.bootstrap4.min.css">
-  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/modules/select2/dist/css/select2.min.css">
-  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/modules/jquery-selectric/selectric.css">
-<?php }?>
+  <?php } elseif ($this->uri->segment(2) == "master"){ ?>
+    <link href="https://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/modules/datatables/datatables.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/modules/datatables/DataTables-1.10.16/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/modules/select2/dist/css/select2.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/modules/jquery-selectric/selectric.css">
+  <?php }?>
 
   <!-- Template CSS -->
   <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
   <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/components.css">
-<!-- Start GA -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
-<script>
+  <!-- Start GA -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
+  <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
@@ -44,8 +44,8 @@ if ($this->uri->segment(2) == "" || $this->uri->segment(2) == "dashboard") { ?>
 </head>
 <?php
 // if ($this->uri->segment(1) == "admin") {
-  // $this->load->view('admin/layout');
-  // $this->load->view('admin/sidebar');
+// $this->load->view('admin/layout');
+// $this->load->view('admin/sidebar');
 // }
 ?>
 
@@ -71,24 +71,25 @@ if ($this->uri->segment(2) == "" || $this->uri->segment(2) == "dashboard") { ?>
                     <input type="password" class="form-control" placeholder="Password" name="password">
                     <span class="glyphicon glyphicon-lock form-control-feedback"></span>
                   </div>
-                  <div class="row">
-                  <div class="col col-xs-12">
-                      <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
-                    </div>
+                  <!-- <div class="row"> -->
+                  <div class="form-group has-feedback">
+                    <!-- <div class="col col-md-12"> -->
+                      <button type="submit" class="btn btn-primary btn-block btn-flat form-control">Masuk</button>
+                    <!-- </div> -->
                     <div class="col col-md-12" style="margin-top:4px; margin-bottom:4px;"></div>
-                    <div class="col col-xs-12  text-center">
+                    <div class="col col-md-12  text-center">
                       <a class="btn" href="<?php echo base_url('Daftar'); ?>">Belum punya akun ? Daftar Disini</a>
                     </div>
                   </div>
-                  </div>
-                  </div>
-                </form>
+                </div>
               </div>
-            </div>
+            </form>
           </div>
         </div>
       </div>
-    </section>
+    </div>
   </div>
+</section>
+</div>
 
 <?php $this->load->view('admin/js'); ?>
